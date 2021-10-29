b7558df452a314c43e094973510215544d845ef5
David Oliveira <davi.exe@hotmail.com>
2021-10-28 22:59:19 -0300

    Inserido mensagens de erros para validação de tamnhao de campos - visíveis através de aplicativos HTTP (Postman)

--- ProjetoAPI/Models/Produto.cs
+++ ProjetoAPI/Models/Produto.cs
@@ -13,39 +13,46 @@ namespace ProjetoAPI.Models
         //espelho de campos da tabela Produtos do DB MySql
        
         public int Id { get; set; }
-       
-        [MaxLength(45), MinLength(3)]
+
+        [MaxLength(45, ErrorMessage = "Tamanho Máximo 45 Caracteres")]
+        [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
         [Column (TypeName ="varchar")]
         public string nome { get; set; }
-       
-        [MaxLength(3), MinLength(1)]
+
+        [MaxLength(3, ErrorMessage = "Tamanho Máximo 3 Caracteres")]
+        [MinLength(1, ErrorMessage = "Tamanho Mínimo 1 carateres")]
         [Column(TypeName = "varchar")]
         public string tamanho { get; set; }
 
-        [MaxLength(45), MinLength(3)]
+        [MaxLength(45, ErrorMessage = "Tamanho Máximo 45 Caracteres")]
+        [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
         [Column(TypeName = "varchar")]
         public string categoria { get; set; }
 
-        [MaxLength(16), MinLength(3)]
+        [MaxLength(16, ErrorMessage = "Tamanho Máximo 16 Caracteres")]
+        [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
         [Column(TypeName = "varchar")]
         public string divisao { get; set; }
 
-        [MaxLength(250), MinLength(3)]
+        [MaxLength(250, ErrorMessage = "Tamanho Máximo 250 Caracteres")]
+        [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
         [Column(TypeName = "varchar")]
         public string descricao { get; set; }
 
         [Column(TypeName = "integer")]
         public int estoque { get; set; }
 
-        [MaxLength(45), MinLength(3)]
+        [MaxLength(45, ErrorMessage ="Tamanho Máximo 45 Caracteres")]
+        [MinLength(3, ErrorMessage ="Tamanho Mínimo 3 carateres")]
         [Column(TypeName = "varchar")]
         public string fornecedor { get; set; }
 
-        [MaxLength(300)]
+        [MaxLength(300, ErrorMessage = "Tamanho Máximo 300 Caracteres")]
         [Column(TypeName = "varchar")]
         public string imagem { get; set; }
 
-        [MaxLength(45), MinLength(3)]
+        [MaxLength(45, ErrorMessage = "Tamanho Máximo 45 Caracteres")]
+        [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
         [Column(TypeName = "varchar")]
         public string usuario_alt { get; set; }
 
