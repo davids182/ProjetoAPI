1b113fbaa4eb223a22772f544e7f95b1ae9c8596
david.oliveira <davi.exe@hotmail.com>
2021-10-29 16:42:21 -0300

    Correção de grafia padrão para Classe Produto Correção de Annotations Correção de nomes de campos para as páginas do CRUD Correção de Migrations na API

---
+++ ProjetoAPI/Migrations/20211029175237_initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoAPI.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    tamanho = table.Column<string>(type: "varchar(3)", maxLength: 3, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    categoria = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    divisao = table.Column<string>(type: "varchar(16)", maxLength: 16, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    descricao = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    estoque = table.Column<int>(type: "integer", nullable: false),
+                    fornecedor = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    imagem = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    usuario_alt = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    reg_alt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
--- ProjetoAPI/Models/Produto.cs
+++ ProjetoAPI/Models/Produto.cs
@@ -9,54 +9,56 @@ namespace ProjetoAPI.Models
 {
     public class Produto
     {
-
+        private readonly string iDnum = "123";
         //espelho de campos da tabela Produtos do DB MySql
        
+        [Column("id")]
         public int Id { get; set; }
 
         [MaxLength(45, ErrorMessage = "Tamanho Máximo 45 Caracteres")]
         [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
-        [Column (TypeName ="varchar")]
-        public string nome { get; set; }
-
+        [Column("nome", TypeName = "varchar")]
+        public string Nome { get; set; }
+        
         [MaxLength(3, ErrorMessage = "Tamanho Máximo 3 Caracteres")]
         [MinLength(1, ErrorMessage = "Tamanho Mínimo 1 carateres")]
-        [Column(TypeName = "varchar")]
-        public string tamanho { get; set; }
+        [Column("tamanho", TypeName = "varchar")]
+        public string Tamanho { get; set; }
 
         [MaxLength(45, ErrorMessage = "Tamanho Máximo 45 Caracteres")]
         [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
-        [Column(TypeName = "varchar")]
-        public string categoria { get; set; }
+        [Column("categoria", TypeName = "varchar")]
+        public string Categoria { get; set; }
 
         [MaxLength(16, ErrorMessage = "Tamanho Máximo 16 Caracteres")]
         [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
-        [Column(TypeName = "varchar")]
-        public string divisao { get; set; }
+        [Column("divisao", TypeName = "varchar")]
+        public string Divisao { get; set; }
 
         [MaxLength(250, ErrorMessage = "Tamanho Máximo 250 Caracteres")]
         [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
-        [Column(TypeName = "varchar")]
-        public string descricao { get; set; }
+        [Column("descricao", TypeName = "varchar")]
+        public string Descricao { get; set; }
 
-        [Column(TypeName = "integer")]
-        public int estoque { get; set; }
+        [Column("estoque", TypeName = "integer")]
+        public int Estoque { get; set; }
 
         [MaxLength(45, ErrorMessage ="Tamanho Máximo 45 Caracteres")]
         [MinLength(3, ErrorMessage ="Tamanho Mínimo 3 carateres")]
-        [Column(TypeName = "varchar")]
-        public string fornecedor { get; set; }
+        [Column("fornecedor", TypeName = "varchar")]
+        public string Fornecedor { get; set; }
 
         [MaxLength(300, ErrorMessage = "Tamanho Máximo 300 Caracteres")]
-        [Column(TypeName = "varchar")]
-        public string imagem { get; set; }
+        [Column("imagem", TypeName = "varchar")]
+        public string Imagem { get; set; }
 
         [MaxLength(45, ErrorMessage = "Tamanho Máximo 45 Caracteres")]
         [MinLength(3, ErrorMessage = "Tamanho Mínimo 3 carateres")]
-        [Column(TypeName = "varchar")]
-        public string usuario_alt { get; set; }
+        [Column("usuario_alt", TypeName = "varchar")]
+        public string Usuario_alt { get; set; }
 
-        public DateTime reg_alt { get; set; } = DateTime.Now;
+        [Column("reg_alt")]
+        public DateTime Reg_alt { get; set; } = DateTime.Now;
 
     }
 }
